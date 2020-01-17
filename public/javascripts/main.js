383901051100b02e075d3acfbf4c852d8f97013d
corey2604 <coreyjduffy@hotmail.com>
2020-01-17 19:46:05 +0000

    Adding initial styling

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var App = React.createClass({
-    render: function() {
-        return(
-            <h1>Hello</h1>
-        )
+class Hello extends React.Component {
+    render() {
+        return <div>Hello {this.props.toWhat}</div>;
     }
-});
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    React.createElement(Hello, {toWhat: 'World'}, null),
+    document.getElementById('root')
+);
