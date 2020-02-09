ad592808e0163bb7d92634cc1cbbfd956f7a2e26
corey2604 <coreyjduffy@hotmail.com>
2020-02-09 15:08:10 +0000

    Adding qualifications section to KSA form

--- app/controllers/KsaFormController.java
+++ app/controllers/KsaFormController.java
@@ -1,12 +1,23 @@
 package controllers;
 
+import models.KsaForm;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
 
+import javax.inject.Inject;
+
 public class KsaFormController extends Controller {
+    private FormFactory formFactory;
+
+    @Inject
+    public KsaFormController(FormFactory formFactory) {
+        this.formFactory = formFactory;
+    }
 
     public Result loadForm(Http.Request request) {
+        KsaForm jobDescription = formFactory.form(KsaForm.class).bindFromRequest().get();
         return ok(views.html.candidate.ksaForm.render());
     }
 }
---
+++ app/models/KsaForm.java
@@ -0,0 +1,72 @@
+package models;
+
+import java.util.List;
+
+public class KsaForm {
+    private String qualificationLevel;
+    private List<String> communicationSkills;
+    private List<String> peopleSkills;
+    private List<String> financialKnowledgeAndSkills;
+    private List<String> thinkingAndAnalysis;
+    private List<String> creativeOrInnovative;
+    private List<String> administrativeOrOrganisational;
+
+    public KsaForm() {
+    }
+
+    public String getQualificationLevel() {
+        return qualificationLevel;
+    }
+
+    public void setQualificationLevel(String qualificationLevel) {
+        this.qualificationLevel = qualificationLevel;
+    }
+
+    public List<String> getCommunicationSkills() {
+        return communicationSkills;
+    }
+
+    public void setCommunicationSkills(List<String> communicationSkills) {
+        this.communicationSkills = communicationSkills;
+    }
+
+    public List<String> getPeopleSkills() {
+        return peopleSkills;
+    }
+
+    public void setPeopleSkills(List<String> peopleSkills) {
+        this.peopleSkills = peopleSkills;
+    }
+
+    public List<String> getFinancialKnowledgeAndSkills() {
+        return financialKnowledgeAndSkills;
+    }
+
+    public void setFinancialKnowledgeAndSkills(List<String> financialKnowledgeAndSkills) {
+        this.financialKnowledgeAndSkills = financialKnowledgeAndSkills;
+    }
+
+    public List<String> getThinkingAndAnalysis() {
+        return thinkingAndAnalysis;
+    }
+
+    public void setThinkingAndAnalysis(List<String> thinkingAndAnalysis) {
+        this.thinkingAndAnalysis = thinkingAndAnalysis;
+    }
+
+    public List<String> getCreativeOrInnovative() {
+        return creativeOrInnovative;
+    }
+
+    public void setCreativeOrInnovative(List<String> creativeOrInnovative) {
+        this.creativeOrInnovative = creativeOrInnovative;
+    }
+
+    public List<String> getAdministrativeOrOrganisational() {
+        return administrativeOrOrganisational;
+    }
+
+    public void setAdministrativeOrOrganisational(List<String> administrativeOrOrganisational) {
+        this.administrativeOrOrganisational = administrativeOrOrganisational;
+    }
+}
