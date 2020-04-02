6726bc4cdf3bf03eded6bb4c17a17a500e661a7a
corey2604 <coreyjduffy@hotmail.com>
2020-04-02 21:29:33 +0100

    Updating Test structure for register test

--- test/controllers/RegisterControllerTest.java
+++ test/controllers/RegisterControllerTest.java
@@ -2,7 +2,9 @@ package controllers;
 
 import com.typesafe.config.Config;
 import models.UserAccountDetails;
+import org.junit.After;
 import org.junit.Test;
+import org.mockito.Mock;
 import play.data.FormFactory;
 import play.mvc.Http;
 import play.mvc.Result;
@@ -14,10 +16,12 @@ import static play.mvc.Http.Status.OK;
 
 public class RegisterControllerTest {
 
+    @Mock
+    private FormFactory mockFormFactory;
+
     @Test
     public void testRegister() {
         //given
-        FormFactory mockFormFactory = mock(FormFactory.class);
         Http.RequestImpl request = Helpers.fakeRequest()
                 .cookie(Http.Cookie.builder("username", "fakeName").build())
                 .cookie(Http.Cookie.builder("userType", "candidate").build())
@@ -31,5 +35,10 @@ public class RegisterControllerTest {
         assertEquals(OK, result.status());
         assertEquals("text/html", result.contentType().get());
         assertEquals("utf-8", result.charset().get());
+    }
+
+    @After
+    public void tearDown() {
+        reset(mockFormFactory);
     }
 }
